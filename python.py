4c50ef8d51ae0a2e738edb1c358752e784bcd225
Mateusz <matrix.05@op.pl>
2022-11-24 23:00:50 +0100

    second part of the changes

--- python.py
+++ python.py
@@ -1,39 +1,12 @@
-from ctypes import sizeof
 import turtle
 import time
 
-window = turtle.Screen()
-boardSide = 600
-X = -300
-Y = 300
-
-window.setup(boardSide, boardSide)
-window.title("Kółko i krzyżyk")
-window.bgcolor('black')
-
-turtle.color('white')
-turtle.pensize(7)
-turtle.speed(0)
-turtle.hideturtle()
-
-tablica = [[None, None, None],
-           [None, None, None],
-           [None, None, None]]
-
-spaceBetweenSquares = int(boardSide / 3)
-
-for a in range(3):
-    turtle.penup()
-    turtle.goto(X + a*spaceBetweenSquares, Y)
-    turtle.pendown()
-    turtle.goto(X + a*spaceBetweenSquares, -Y)
-
-    turtle.penup()
-    turtle.goto(X, Y - a*spaceBetweenSquares)
-    turtle.pendown()
-    turtle.goto(-X, Y - a*spaceBetweenSquares)
-
-orderInput = turtle.textinput("Wybierz", "kółko/krzyżyk")
+def orderXO():
+    global orderInput
+    if orderInput == 'kółko':
+        orderInput = 'krzyżyk'
+    else:
+        orderInput = 'kółko'
 
 def paintingOX():
     if orderInput == 'krzyżyk':
@@ -47,39 +20,31 @@ def paintingOX():
         turtle.write('Niepoprawny wzór', font=("Arial", 50))
         turtle.bye()
 
-def orderXO():
-    global orderInput
-    if orderInput == 'kółko':
-        orderInput = 'krzyżyk'
-    else:
-        orderInput = 'kółko'
-
 def checkingWhoWin():
-
     # rows for circle
     for w in range(3):
-        if (tablica[w][0] == "kółko" and tablica[w][1] == "kółko" and  tablica[w][2] == "kółko"):
-            return tablica[w][2]
+        if (array[w][0] == "kółko" and array[w][1] == "kółko" and array[w][2] == "kółko"):
+            return array[w][2]
 
     # rows for cross
     for w in range(3):
-        if (tablica[w][0] == "krzyżyk" and tablica[w][1] == "krzyżyk" and tablica[w][2] == "krzyżyk"):
-            return tablica[w][2]
+        if (array[w][0] == "krzyżyk" and array[w][1] == "krzyżyk" and array[w][2] == "krzyżyk"):
+            return array[w][2]
 
     # columns for circle
     for k in range(3):
-        if (tablica[0][k] == "kółko" and tablica[1][k] == "kółko"  and tablica[2][k] == "kółko"):
-            return tablica[2][k]
+        if (array[0][k] == "kółko" and array[1][k] == "kółko" and array[2][k] == "kółko"):
+            return array[2][k]
 
     # columns for cross
     for k in range(3):
-        if (tablica[0][k] == "krzyżyk" and tablica[1][k] == "krzyżyk" and tablica[2][k] == "krzyżyk"):
-            return tablica[2][k]
+        if (array[0][k] == "krzyżyk" and array[1][k] == "krzyżyk" and array[2][k] == "krzyżyk"):
+            return array[2][k]
 
-    if tablica[0][0] == tablica[1][1] and tablica[0][0] == tablica[2][2]:
-        return tablica[2][2]
-    if tablica[0][2] == tablica[1][1] and tablica[0][2] == tablica[2][0]:
-        return tablica[2][0]
+    if array[0][0] == array[1][1] and array[0][0] == array[2][2]:
+        return array[2][2]
+    if array[0][2] == array[1][1] and array[0][2] == array[2][0]:
+        return array[2][0]
 
 checkingWinner = checkingWhoWin
 
@@ -96,8 +61,6 @@ def writingOutTheWinner():
         movingTurtleAfterTheWin()
 
 def runGame(x, y):
-
-    global orderInput
     column = 0
     row = 0
 
@@ -115,20 +78,54 @@ def runGame(x, y):
     else:
         row = 1
 
-    if tablica[row][column] != None:
-        return tablica
+    if array[row][column] != None:
+        return array
 
-    centerOfColumn = (column*spaceBetweenSquares + spaceBetweenSquares/2) - boardSide / 2
-    centerOfRow = (-row*spaceBetweenSquares - spaceBetweenSquares/2) + boardSide / 2
+    centerOfColumn = (column*spaceBetweenSquares +
+                      spaceBetweenSquares/2) - boardSide / 2
+    centerOfRow = (-row*spaceBetweenSquares -
+                   spaceBetweenSquares/2) + boardSide / 2
 
     turtle.penup()
     turtle.goto(centerOfColumn-25, centerOfRow-25)
 
-    tablica[row][column] = orderInput
+    array[row][column] = orderInput
 
     paintingOX()
     orderXO()
     writingOutTheWinner()
 
+
+if __name__ == "__main__":
+    window = turtle.Screen()
+    boardSide = 600
+    X = -300
+    Y = 300
+
+    window.setup(boardSide, boardSide)
+    window.title("Kółko i krzyżyk")
+    window.bgcolor('black')
+
+    turtle.color('white')
+    turtle.pensize(7)
+    turtle.speed(0)
+    turtle.hideturtle()
+
+    spaceBetweenSquares = int(boardSide / 3)
+
+    array = [[None, None, None], [None, None, None], [None, None, None]]
+
+    for a in range(3):
+        turtle.penup()
+        turtle.goto(X + a*spaceBetweenSquares, Y)
+        turtle.pendown()
+        turtle.goto(X + a*spaceBetweenSquares, -Y)
+
+        turtle.penup()
+        turtle.goto(X, Y - a*spaceBetweenSquares)
+        turtle.pendown()
+        turtle.goto(-X, Y - a*spaceBetweenSquares)
+
+    orderInput = turtle.textinput("Wybierz", "kółko/krzyżyk")
 window.onclick(runGame)
 turtle.mainloop()
